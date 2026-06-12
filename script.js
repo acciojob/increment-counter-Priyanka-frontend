b6c9c22aeeef3856a49ee08dad34c54d1d86bfd1
Priyanka-frontend <kaushikpriyanka005@gmail.com>
2026-06-12 11:10:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const counter = document.getElementById("counter");
+const incrementBtn = document.getElementById("incrementBtn");
+
+incrementBtn.onclick = function () {
+    alert(counter.innerText);
+    counter.innerText = Number(counter.innerText) + 1;
+};
